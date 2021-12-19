986fff39e00d066530b70c2ea7894850c9f99266
SantiSL5 <santiagosolerllin@gmail.com>
2021-12-19 18:51:46 +0100

    test badge change

--- .github/actions/badge_readme/script.sh
+++ .github/actions/badge_readme/script.sh
@@ -8,9 +8,9 @@ line=`grep -n '^<!---Start place for the badge -->' README.md  | cut -d":" -f1`
 (( line++ ))
 
 if [[ $1 == "success" ]]; then
-    awk 'NR=='"${line}"'{$0="[![Cypress.io](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)](https://www.cypress.io/)"} 1' README.md > README.tmp && mv README.tmp README.md
+    awk 'NR=='"${line}"'{$0="RESULTADO DE LOS ÚLTIMOS TESTS: [![Cypress.io](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)](https://www.cypress.io/)"} 1' README.md > README.tmp && mv README.tmp README.md
 else
-    awk 'NR=='"${line}"'{$0="[![Cypress.io](https://img.shields.io/badge/test-failure-red)](https://www.cypress.io/)"} 1' README.md > README.tmp && mv README.tmp README.md
+    awk 'NR=='"${line}"'{$0="RESULTADO DE LOS ÚLTIMOS TESTS: [![Cypress.io](https://img.shields.io/badge/test-failure-red)](https://www.cypress.io/)"} 1' README.md > README.tmp && mv README.tmp README.md
 fi
 
 
